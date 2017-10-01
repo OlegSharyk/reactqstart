530160729d002ec18cda5ae316b133aeb74f9783
docshark <docshark@gmail.com>
2017-10-01 18:50:39 +0300

    added boiler old version not worked

---
+++ 5/js/source/components/Logo.js
@@ -0,0 +1,17 @@
+//var React = require('react');
+//var Logo = React.createClass({
+//    render: function(){
+//        return <div className="Logo" />;
+//    }
+//});
+//module.exports = Logo;
+
+import React from 'react';
+
+class Logo extends React.Component {
+    render(){
+        return <div classNames="Logo" />
+    }
+}
+
+export default Logo
